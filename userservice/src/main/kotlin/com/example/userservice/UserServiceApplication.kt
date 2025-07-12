4ff10cb372f76d170460de1ab0cd9fb0a3dc8efd
Yongha Kwon <yongha.kwon12@gmail.com>
2025-07-12 23:04:42 +0900

    init

--- userservice/src/main/kotlin/com/example/userservice/UserServiceApplication.kt
+++ userservice/src/main/kotlin/com/example/userservice/UserServiceApplication.kt
@@ -2,10 +2,12 @@ package com.example.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-class UserserviceApplication
+@EnableDiscoveryClient
+class UserServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<UserserviceApplication>(*args)
+	runApplication<UserServiceApplication>(*args)
 }
