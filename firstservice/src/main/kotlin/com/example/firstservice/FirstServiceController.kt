acd7ab4ef196c2f2d4bf2f7a080f2626d5a13a85
Yongha Kwon <yongha.kwon12@gmail.com>
2025-07-13 23:54:01 +0900

    feat: 섹셕3 수강 중
    API gateway 추가.

---
+++ firstservice/src/main/kotlin/com/example/firstservice/FirstServiceController.kt
@@ -0,0 +1,53 @@
+package com.example.firstservice
+
+import jakarta.servlet.http.HttpServletRequest
+import lombok.extern.slf4j.Slf4j
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/first-service")
+@Slf4j
+class FirstServiceController(var env: Environment) {
+
+    private val log = logger()
+    @GetMapping("/welcome")
+    fun welcome(): String {
+        return "Welcome to the First service."
+    }
+
+    @GetMapping("/message")
+    fun message(@RequestHeader("first-request") header: String?): String {
+        log.info(header)
+        return "Hello World in First Service."
+    }
+
+    @GetMapping("/check")
+    fun check(request: HttpServletRequest): String {
+        val headers: Enumeration<String> = request.headerNames
+        Collections.list(headers).stream().forEach { name: String ->
+            val values: Enumeration<String> = request.getHeaders(name)
+            Collections.list(values).stream().forEach { value: String -> println("$name=$value") }
+        }
+
+        log.info("Server port={}", request.serverPort)
+
+        log.info(
+            "spring.cloud.client.hostname={}",
+            env.getProperty("spring.cloud.client.hostname")
+        )
+        log.info(
+            "spring.cloud.client.ip-address={}",
+            env.getProperty("spring.cloud.client.ip-address")
+        )
+
+        return String.format(
+            "Hi, there. This is a message from First Service on PORT %s",
+            env.getProperty("local.server.port")
+        )
+    }
+}
---
+++ secondservice/src/main/kotlin/com/example/secondservice/SecondServiceController.kt
@@ -0,0 +1,31 @@
+package com.example.secondservice
+
+import lombok.extern.slf4j.Slf4j
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/second-service")
+@Slf4j
+class SecondServiceController {
+
+    private val log = logger()
+
+    @GetMapping("/welcome")
+    fun welcome(): String {
+        return "Welcome to the Second service."
+    }
+
+    @GetMapping("/message")
+    fun message(@RequestHeader("second-request") header: String?): String {
+        log.info(header)
+        return "Hello World in Second Service."
+    }
+
+    @GetMapping("/check")
+    fun check(): String {
+        return "Hi, there. This is a message from Second Service."
+    }
+}
